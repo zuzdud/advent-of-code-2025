70bef7cc7511f72f60b93c4b944da4e56d026af5
zuzuz <duda.zuzanna7@gmail.com>
2026-05-23 18:40:12 +0200

    shitty day 4 second puzzle done

--- day-04/main.py
+++ day-04/main.py
@@ -1,11 +1,12 @@
 with open("day-04/my_input.txt") as file:
-    rolls_grid = [line.rstrip() for line in file]
+    rolls_grid = [list(line.rstrip()) for line in file]
 
 offset_map = {
     1: (-1, -1), 2: (-1, 0),  3: (-1, 1),
     8: (0, -1),               4: (0, 1),
     7: (1, -1),  6: (1, 0),   5: (1, 1)
 }
+
 
 def check_neighbour(x, y, directions):
     counter = 0
@@ -31,25 +32,50 @@ def can_be_accessed(x, y):
         return True
 
     if x == 0:
-        return check_neighbour(int(x), int(y), [4, 5, 6, 7, 8])
+        return check_neighbour(x, y, [4, 5, 6, 7, 8])
 
     if x == len(rolls_grid)-1:
-        return check_neighbour(int(x), int(y), [4, 1, 2, 3, 8])
+        return check_neighbour(x, y, [4, 1, 2, 3, 8])
 
     if y == 0:
-        return check_neighbour(int(x), int(y), [2, 3, 4, 5, 6])
+        return check_neighbour(x, y, [2, 3, 4, 5, 6])
 
     if y == len(rolls_grid[0])-1:
-        return check_neighbour(int(x), int(y), [2, 1, 8, 7, 6])
+        return check_neighbour(x, y, [2, 1, 8, 7, 6])
 
-    return check_neighbour(int(x), int(y), [1, 2, 3, 4, 5, 6, 7, 8])
+    return check_neighbour(x, y, [1, 2, 3, 4, 5, 6, 7, 8])
 
 
-rolls_counter = 0
-for x in range(0, len(rolls_grid)):
-    for y in range(0, len(rolls_grid[0])):
-        if rolls_grid[x][y] == "@":
-            if can_be_accessed(int(x), int(y)):
-                rolls_counter += 1
+# rolls_counter = 0
+# for x in range(0, len(rolls_grid)):
+#     for y in range(0, len(rolls_grid[0])):
+#         if rolls_grid[x][y] == "@":
+#             if can_be_accessed(x, y):
+#                 rolls_counter += 1
 
-print(rolls_counter)
+def pick_rolls():
+    rolls_counter = 0
+    picked_rolls = []
+    for x in range(0, len(rolls_grid)):
+        for y in range(0, len(rolls_grid[0])):
+            if rolls_grid[x][y] == "@":
+                if can_be_accessed(x, y):
+                    picked_rolls.append([x, y])
+                    rolls_counter += 1
+    # prpcked_rolls)
+
+    for xy in picked_rolls:
+        rolls_grid[xy[0]][xy[1]] = "."
+
+    return rolls_counter
+
+
+rolls_picked = 0
+while True:
+    rolls = pick_rolls()
+    rolls_picked += rolls
+    if rolls == 0:
+        break
+
+
+print(rolls_picked)
